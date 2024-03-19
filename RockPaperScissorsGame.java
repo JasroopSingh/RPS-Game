7115c1097dde6ef065166e4c3e59b32dd6a3807c
JasroopSingh <jasroopsingh001@gmail.com>
2024-03-18 19:10:49 -0500

    running

--- RockPaperScissorsGame.java
+++ RockPaperScissorsGame.java
@@ -1,78 +1,110 @@
 import java.util.Scanner; // import the java.util.Scanner class, allows you to take user input 
 import java.util.Random; // import the java.util.Random class, allows you to generate random numbers/other vaules
 
+
 public class RockPaperScissorsGame 
 {
-    private String[] validOptions = {"rock", "paper", "scissors"};
-    private Random random = new Random();
-    private Scanner scanner = new Scanner(System.in);
+    private String[] validOptions = {"rock", "paper", "scissors"}; // Array of valid options rock, paper, scissors
+    private Random random = new Random(); //used random for generating random numbers, used by bot to choose play from 
+    private Scanner scanner = new Scanner(System.in); // used scanner for taking user input
 
-    public RockPaperScissorsGame() {}
-    
-    public void run() 
+    public RockPaperScissorsGame() {} // Constructor for RockPaperScissorsGame
+
+    public void run() // function to run the Rock Paper Scissors game
     {
-        int totalRounds = 3;
-        int currentRound = 0;
+        int totalRounds = 3; //Total Rounds that can be played Player vs Bot
+        int currentRound = 0; // Current round number
+
+        // Number of wins for player and bot
         int playerWins = 0;
-        int computerWins = 0;
+        int botWins = 0;
+
+        System.out.println("***********************");
+        System.out.println("* Welcome to the Game *");
+        System.out.println("***********************");
 
         while (currentRound < totalRounds) 
         {
-            System.out.println("Chose one of the following plays:\n 1. Rock \n 2. Paper \n 3. Scissors \n");
-            String playerChoose = scanner.next().trim().toLowerCase();
-            String computerGuess = validOptions[random.nextInt(3)];
-            
-            System.out.println("Player attacked with: " + playerChoose);
-            System.out.println("Bot attacked with: " + computerGuess); 
+            System.out.print("Please type your choose from the following:\n - Rock \n - Paper \n - Scissors \n\nInput: "); // Prompt user for input
+            String playerChoose = scanner.next().trim().toLowerCase(); // Get user input, .trim() used to remove extra spaces, .toLowerCase() used to lower case all user input
+            String botChoose = validOptions[random.nextInt(3)]; // Generate random bot choice for the game
 
-            if (playerChoose.equals(computerGuess)) 
+            if (playerChoose.equals(botChoose)) 
             {
-                System.out.println("Computer guessed that too.\n");
-                continue;
+                System.out.println("Bot guessed that too.\n"); // Inform about the tie
             } 
-            else if (playerChoose.equals("rock") && computerGuess.equals("paper")) 
+            else if (playerChoose.equals("rock") && botChoose.equals("paper")) 
             {
-                computerWins++;
-                System.out.println("Computer wins: paper beats rock\n");
+                System.out.println("Player chosse: " + playerChoose);
+                System.out.println("Bot chosse: " + botChoose);
+                System.out.println();
+                botWins++;
+                System.out.println("Bot wins: paper beats rock\n");
             } 
-            else if (playerChoose.equals("rock") && computerGuess.equals("scissors")) 
+            else if (playerChoose.equals("rock") && botChoose.equals("scissors")) 
             {
+                System.out.println("Player chosse: " + playerChoose);
+                System.out.println("Bot chosse: " + botChoose);
+                System.out.println();
                 playerWins++;
                 System.out.println("Player wins: rock beats scissors\n");
             } 
-            else if (playerChoose.equals("paper") && computerGuess.equals("rock")) 
+            else if (playerChoose.equals("paper") && botChoose.equals("rock")) 
             {
+                System.out.println("Player chosse: " + playerChoose);
+                System.out.println("Bot chosse: " + botChoose);
+                System.out.println();
                 playerWins++;
                 System.out.println("Player wins: paper beats rock\n");
             } 
-            else if (playerChoose.equals("paper") && computerGuess.equals("scissors")) 
+            else if (playerChoose.equals("paper") && botChoose.equals("scissors")) 
             {
-                computerWins++;
-                System.out.println("Computer wins: scissors beats paper\n");
+                System.out.println("Player chosse: " + playerChoose);
+                System.out.println("Bot chosse: " + botChoose);
+                System.out.println();
+                botWins++;
+                System.out.println("Bot wins: scissors beats paper\n");
             } 
-            else if (playerChoose.equals("scissors") && computerGuess.equals("rock")) 
+            else if (playerChoose.equals("scissors") && botChoose.equals("rock")) 
             {
-                computerWins++;
-                System.out.println("Computer wins: rock beats scissors\n");
+                System.out.println("Player chosse: " + playerChoose);
+                System.out.println("Bot chosse: " + botChoose);
+                System.out.println();
+                botWins++;
+                System.out.println("Bot wins: rock beats scissors\n");
             } 
-            else if (playerChoose.equals("scissors") && computerGuess.equals("paper")) 
+            else if (playerChoose.equals("scissors") && botChoose.equals("paper")) 
             {
+                System.out.println("Player chosse: " + playerChoose);
+                System.out.println("Bot chosse: " + botChoose);
+                System.out.println();
                 playerWins++;
                 System.out.println("Player wins: scissors beats paper\n");
             } 
-            else {
-                System.out.println("Invalid input.\n");
+            
+            else 
+            {
+                System.out.print("Invalid input (" + playerChoose + ")" + "Please type Rock, Paper or Scissors \n\n");
+                currentRound--;
             }
+            
             currentRound++;
-        } // END OF WHILE LOOP
+            
+        
+            if (currentRound >= totalRounds)
+                {
+                    System.out.println("Game over. Player " + playerWins + " - " + botWins + " Computer"); // Print game result
+                    System.out.print("Play again? (Y/N): "); // Prompt for replay
+                    String playAgain = scanner.next().trim().toLowerCase(); // Get input for players decision
 
-            System.out.println("Game over. Player " + playerWins + " - " + computerWins + " Computer");
-            System.out.print("Play again? (Y/N): ");
-            String playAgain = scanner.next().trim().toLowerCase();
-            if (!playAgain.equals("y")) {
-                return;
-            } else {
-                run(); // Restart the game
-            }
+                    if (playAgain.equals("y")) {run();} // Restart the game
+
+                    else 
+                    {
+                    System.out.println("Thanks for playing! Goodbye.");
+                    System.exit(0); // Exit the game
+                    }
+                }
+        }
     }
 }
